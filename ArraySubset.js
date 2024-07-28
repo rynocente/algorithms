449df1083217f80e364c227c041955ecb84e9e38
Ruben <rubenynocente@hotmail.com>
2024-07-28 01:06:24 -0500

    hastable

---
+++ ArraySubset.js
@@ -0,0 +1,39 @@
+array1 = ["a","b","c","d","e","f"];
+array2 = ["b","d","f"];
+
+let rpta = isSubset(array1, array2);
+console.log("rpta: " + rpta);
+
+function isSubset(array1, array2) {
+
+    let largerArray;
+    let smallerArray;
+    let hashtable = {};
+
+    if(array1.length > array2.length) {
+        largerArray = array1;
+        smallerArray = array2;
+    }else {
+        largerArray = array2;
+        smallerArray = array1;
+    }
+
+    console.log("larger: " + largerArray);
+    console.log("smaller: " + smallerArray);
+
+
+    //store hashtable
+    for(const key of largerArray) {
+        hashtable[key] = true;
+    }
+
+    for(const key of smallerArray) {
+        if(!hashtable[key]) {
+            return false;
+        }
+    }
+
+    return true;
+
+
+}
--- README.md
+++ README.md
@@ -5,7 +5,11 @@ Some algorithms in Java and Javascript :knife: :knife: :black_heart:
 - O(N) if there are N data elements, how many steps will the algorithm take?
 - Big O Notation ignores constants
 - Big O Notation only takes into account the highest order of N when we have multiple orders added together
-
+- Big O focuses primarily on worst-case scenarios
+- O(N * M) can be constructed as a range between O(N) and O(N²)
+- Looking up a value in a hash table has an efficency of O(1) on average, as it usually takes just one step
+- A good hash table strikes a balance of avoiding collisions while not consuming lots of memory
+        
 
 
 
@@ -14,6 +18,8 @@ O(1)
 O(log N)
 
 O(N)
+
+O(N*M)
 
 O(N²)
 
